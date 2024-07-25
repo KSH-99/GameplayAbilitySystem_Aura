eacb0e1b5a1d672c37973e6a4b66e67a67483f3a
KSH-99 <kk00011157@gmail.com>
2024-07-25 16:54:31 +0900

    HighlightEnemy function definition

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -5,8 +5,10 @@
 
 void AAuraEnemy::HighLightActor()
 {
+    bHighlighted = true;
 }
 
 void AAuraEnemy::UnHighLightActor()
 {
+    bHighlighted = false;
 }
--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,10 +4,72 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if(!CursorHit.bBlockingHit) return;
+
+	LastActor = ThisActor;
+	ThisActor = CursorHit.GetActor();
+
+	/**
+	 * Line trace from cursor. There are several scenarios:
+	 *  A. LastActor is null && ThisActor is null
+	 *		- Do nothing
+	 *	B. LastActor is null && ThisActor is valid
+	 *		- Highlight ThisActor.
+	 *	C. LastActor is valid && ThisActor is null
+	 *		- UnHighlight LastActor.
+	 *	D. Both actors are valid, but LastActor != ThisActor
+	 *		- UnHighlight LastActor and Highlight ThisActor.
+	 *	E. Both actors are valid and are the same actor
+	 *		- Do nothing
+	 */
+
+	if(LastActor == nullptr)
+	{
+		if(ThisActor)
+		{
+			//Cast B - Highlight ThisActor.
+			ThisActor->HighLightActor();
+		}else
+		{
+			// Case A - both are null, do nothing
+		}
+	}else // LastActor is valid
+	{
+		if(ThisActor == nullptr)
+		{
+			// Case C - UnHighlight LastActor.
+			LastActor->UnHighLightActor();
+		}else // both actor are valid
+		{
+			if(LastActor != ThisActor)
+			{
+				// Case D - UnHighlight LastActor and Highlight ThisActor.
+				LastActor->UnHighLightActor();
+				ThisActor->HighLightActor();
+			}else
+			{
+				// Case E - Do nothing
+			}
+		}
+	}
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -51,3 +113,5 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 		ControlledPawn->AddMovementInput(RightDirection, InputAxisValue.X);
 	}
 }
+
+
